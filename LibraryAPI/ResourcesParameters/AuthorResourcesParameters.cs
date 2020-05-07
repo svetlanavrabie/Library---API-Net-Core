d970dd2034ca5483b42be2f57648404c0fe0913f
svetlanavrabie <laurasdamir@gmail.com>
2020-05-06 22:28:37 -0400

    feat: Implemented pagination system for AuthorsController (GetAuthors)

--- LibraryAPI/ResourcesParameters/AuthorResourcesParameters.cs
+++ LibraryAPI/ResourcesParameters/AuthorResourcesParameters.cs
@@ -2,8 +2,21 @@
 {
     public class AuthorResourcesParameters
     {
+        const int maxPageSize = 20;
+
         public string MainCategory { get; set; }
 
         public string Search { get; set; }
+
+        public int PageNumber { get; set; } = 1;
+
+
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get => _pageSize;
+
+            set => _pageSize = (value > maxPageSize) ? maxPageSize : value;
+        }
     }
 }
--- LibraryAPI/Services/ICourseLibraryRepository.cs
+++ LibraryAPI/Services/ICourseLibraryRepository.cs
@@ -1,4 +1,5 @@
 ﻿using CourseLibrary.API.Entities;
+using LibraryAPI.Helpers;
 using LibraryAPI.ResourcesParameters;
 using System;
 using System.Collections.Generic;
@@ -8,18 +9,31 @@ namespace CourseLibrary.API.Services
     public interface ICourseLibraryRepository
     {    
         IEnumerable<Course> GetCourses(Guid authorId);
+
         Course GetCourse(Guid authorId, Guid courseId);
+
         void AddCourse(Guid authorId, Course course);
+
         void UpdateCourse(Course course);
+
         void DeleteCourse(Course course);
+
         IEnumerable<Author> GetAuthors();
+
         Author GetAuthor(Guid authorId);
+
         IEnumerable<Author> GetAuthors(IEnumerable<Guid> authorIds);
-        IEnumerable<Author> GetAuthors(AuthorResourcesParameters resourcesParameters);
+       
+        PagedList<Author> GetAuthors(AuthorResourcesParameters resourcesParameters);
+       
         void AddAuthor(Author author);
+        
         void DeleteAuthor(Author author);
+        
         void UpdateAuthor(Author author);
+       
         bool AuthorExists(Guid authorId);
+        
         bool Save();
     }
 }
